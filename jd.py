cbcedb46b01c69679f3947353c334bda0cd2aada
Jeong Yong-uk <ywj1022@gmail.com>
2015-11-16 02:31:41 +0900

    Suggest some candidates
    For example, 'jd.py 3 java' gives you directories which have prefix
    'java*' for depth 3.

--- jd.py
+++ jd.py
@@ -1,11 +1,29 @@
 #! /usr/bin/env python
 
 import sys
+import os
+import glob
 
 def main():
-    if len(sys.argv) == 1:
+    if len(sys.argv) < 3:
         return
-    print sys.argv[1]
+
+    depth = int(sys.argv[1])
+    keyword = sys.argv[2]
+
+    dirnames = getdir('.', keyword, depth)
+    print ' '.join([d[2:] for d in dirnames])
+
+def getdir(root, keyword, depth):
+    if depth <= 0:
+        return []
+
+    result = []
+    if depth > 1:
+        for dir in filter(os.path.isdir, [os.path.join(root, d) for d in os.listdir(root)]):
+            result += getdir(dir, keyword, depth - 1)
+    result += filter(os.path.isdir, glob.glob(os.path.join(root, keyword)+'*'))
+    return result
 
 if __name__ == '__main__':
     main()
